9b84a1d815eaec48c77f6c17d71d5f2b66edaa93
JunWan00036 <nuaapaper@gmail.com>
2023-03-22 00:58:42 -0400

    The first commit, it works fine, can retrieve data from website and display correctly.  but the app is unable to display and save the information correctly, at the same time without fragment

---
+++ app/src/main/java/com/example/final_project/WeatherActivity.java
@@ -0,0 +1,167 @@
+package com.example.final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import android.view.View;
+
+import com.bumptech.glide.Glide;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import android.widget.ImageView;
+public class WeatherActivity extends AppCompatActivity implements WeatherDataListener {
+    private EditText searchEditText;
+    private TextView cityTextView;
+
+    private TextView temperatureTextView;
+    private TextView weatherConditionTextView;
+    private ImageView weatherIconImageView;
+    private WeatherAPI weatherAPI;
+    private TextView searchResultsTextView;
+    private RecyclerView weatherRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+        weatherRecyclerView = findViewById(R.id.result_list);
+        weatherRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        weatherIconImageView = findViewById(R.id.weatherIconImageView);
+
+
+
+        searchEditText = findViewById(R.id.search_edit_text);
+        cityTextView = findViewById(R.id.city_text_view);
+        temperatureTextView = findViewById(R.id.temperature_text_view);
+        weatherConditionTextView = findViewById(R.id.weather_condition_text_view);
+        searchResultsTextView = findViewById(R.id.search_results_text_view);
+
+
+        weatherAPI = new WeatherAPI(this);
+
+        searchEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    String cityName = searchEditText.getText().toString();
+                    weatherAPI.getWeatherDataForCity(cityName, (WeatherDataListener) WeatherActivity.this);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        WeatherData savedWeatherData = weatherAPI.getSavedWeatherData();
+        if (savedWeatherData != null) {
+            updateWeatherData(savedWeatherData);
+        }
+        Button searchButton = findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick (View v){
+                String cityName = searchEditText.getText().toString();
+                weatherAPI.getWeatherDataForCity(cityName, (WeatherDataListener) WeatherActivity.this);
+            }
+        });
+    }
+
+
+
+
+    @Override
+    public void onDataReceived(WeatherData weatherData) {
+        updateWeatherData(weatherData);
+    }
+
+    @Override
+    public void onError(String errorMessage) {
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    private void updateWeatherData(WeatherData weatherData) {
+        cityTextView.setText(weatherData.getCityName());
+        temperatureTextView.setText(String.format(Locale.getDefault(), "%.1f °C", weatherData.getTemperature()));
+        weatherConditionTextView.setText(weatherData.getWeatherCondition());
+
+        String searchResultsText = String.format(Locale.getDefault(), "Search results for \"%s\":\n%s", searchEditText.getText().toString(), weatherData.getWeatherCondition());
+        searchResultsTextView.setText(searchResultsText);
+
+        // Display the weather icon
+        String iconUrl = weatherData.getWeatherIconUrl();
+        if (iconUrl != null && !iconUrl.isEmpty()) {
+            Glide.with(this).load(iconUrl).into(weatherIconImageView);
+        }
+    }
+
+
+
+
+
+    /*private Button weather;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+        weather = findViewById(R.id.search_button);
+
+        weather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getWeather("London", "your_api_key_here");
+            }
+        });
+    }
+
+    public void getWeather(String location, String apiKey) {
+        String url = "http://api.weatherstack.com/current?access_key=" + apiKey + "&query=" + location;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseData = response.body().string();
+                Log.d("Weather data", responseData);
+            }
+        });
+    }*/
+}
